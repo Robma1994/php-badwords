8e61c66089a3395861c83e5e4518ed3b43c6ce5b
Roberto <robertomartinonwg@gmail.com>
2021-10-07 15:04:29 +0200

    parametro GET

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
-<!-- 1.Creare una variabile con un paragrafo di testo a vostra scelta. -->
-<!-- 2.Stampare a schermo il paragrafo e la sua lunghezza. -->
-<!-- 3.Una parola da censurare viene passata dall'utente tramite parametro GET. -->
-<!-- 4.Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) 
-tutte le occorrenze della parola da censurare. -->
 
 <?php
     //Creazione variabile test
     $text = 'tie';
-    $_GET['name']
+    //Parametro Get
+    $keyWord = $_GET['name'];
 ?>
 
 <!-- 2.Stampare a schermo il paragrafo e la sua lunghezza. -->
 <p>Forza Napoli <?php echo $text ?></p>
+<p>La lunghezza del testo è di: <?php echo strlen($text)?> lettere</p>
+
+<!-- 3.Una parola da censurare viene passata dall'utente tramite parametro GET. -->
+<h1><?php echo $keyWord ?></h1>
+
+<!-- 4.Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) 
+tutte le occorrenze della parola da censurare. -->
